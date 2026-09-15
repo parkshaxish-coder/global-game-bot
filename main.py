507dee033011d37134bee46153a71826ed3961ca
parkshaxish-coder <parkshaxish@gmail.com>
2026-09-15 19:26:09 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import logging, asyncio
+import logging, os, asyncio
 from aiogram import Bot, Dispatcher, types, F
 from aiogram.filters import Command
 from aiogram.types import LabeledPrice, PreCheckoutQuery, Message, WebAppInfo
+from aiohttp import web
 
 BOT_TOKEN = "8810572867:AAEleoFb5RH4BW7yY4MGimQOcN8H93dfY8Q"
 logging.basicConfig(level=logging.INFO)
@@ -30,12 +31,21 @@ async def pre_c(q: PreCheckoutQuery):
 async def succ_p(m: Message):
     await m.answer("🎉 **Unlocked!**", reply_markup=types.InlineKeyboardMarkup(inline_keyboard=[[types.InlineKeyboardButton(text="🔥 Launch Tetris", web_app=WebAppInfo(url="https://gamepix.com"))]]))
 
+# Render tekin rejasi so'raydigan majburiy port qismi
+async def h(r):
+    return web.Response(text="Bot is Live")
+
 async def main():
+    app = web.Application()
+    app.router.add_get('/', h)
+    runner = web.AppRunner(app)
+    await runner.setup()
+    # Render so'ragan PORT shu erda avtomatik ochiladi
+    port = int(os.environ.get("PORT", 8080))
+    await web.TCPSite(runner, '0.0.0.0', port).start()
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
     asyncio.run(main())
-
-
 
 
